b41bece402f47212d1a65d348473e54cc84ac45f
Gregor Baumgärtner <baumgaertner_gregor@web.de>
2017-08-28 12:30:03 +0200

    Test Kommentar

--- FES.SchulHardwareAusleihSystem/FES.SchulHardwareAusleihSystem/Interfaces/INutzer.cs
+++ FES.SchulHardwareAusleihSystem/FES.SchulHardwareAusleihSystem/Interfaces/INutzer.cs
@@ -7,6 +7,9 @@ namespace FES.SchulHardwareAusleihSystem.Interfaces
 {
     public interface INutzer
     {
+        /// <summary>
+        /// Dieses Interface beinhaltet alles rund um den Nutzer
+        /// </summary>
         string Name { get; set; }
         INutzerRolle NutzerRolle { get; set; }
         string Email { get; set; }
