ba38095c2dadd54bdf69bfb02b9cdcc556c6cdf1
Gregor Baumgärtner <baumgaertner_gregor@web.de>
2017-08-28 12:28:07 +0200

    Interface Ordner mit Testinterfaces

---
+++ FES.SchulHardwareAusleihSystem/FES.SchulHardwareAusleihSystem/Interfaces/INutzer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FES.SchulHardwareAusleihSystem.Interfaces
+{
+    public interface INutzer
+    {
+        string Name { get; set; }
+        INutzerRolle NutzerRolle { get; set; }
+        string Email { get; set; }
+    }
+}
