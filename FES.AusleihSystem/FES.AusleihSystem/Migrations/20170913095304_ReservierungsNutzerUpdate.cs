3470b97501a2d5e839e59d5e7b2f645e6a0833ad
Gregor Baumgärtner <baumgaertner_gregor@web.de>
2017-09-13 12:07:06 +0200

    Reservierungangepasst

---
+++ FES.AusleihSystem/FES.AusleihSystem/Migrations/20170913095304_ReservierungsNutzerUpdate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FES.AusleihSystem.Migrations
+{
+    public partial class ReservierungsNutzerUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NutzerId",
+                table: "Reservierungen",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservierungen_NutzerId",
+                table: "Reservierungen",
+                column: "NutzerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservierungen_AspNetUsers_NutzerId",
+                table: "Reservierungen",
+                column: "NutzerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservierungen_AspNetUsers_NutzerId",
+                table: "Reservierungen");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservierungen_NutzerId",
+                table: "Reservierungen");
+
+            migrationBuilder.DropColumn(
+                name: "NutzerId",
+                table: "Reservierungen");
+        }
+    }
+}
--- FES.AusleihSystem/FES.AusleihSystem/Migrations/ApplicationDbContextModelSnapshot.cs
+++ FES.AusleihSystem/FES.AusleihSystem/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,85 +17,7 @@ namespace FES.AusleihSystem.Migrations
                 .HasAnnotation("ProductVersion", "1.0.4")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("FES.AusleihSystem.ViewModels.GeraetViewModel", b =>
-                {
-                    b.Property<int>("ID")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<int>("EAN");
-
-                    b.Property<int>("GeraeteStatus");
-
-                    b.Property<string>("Name");
-
-                    b.Property<int?>("ReservierungsNummer");
-
-                    b.HasKey("ID");
-
-                    b.HasIndex("ReservierungsNummer");
-
-                    b.ToTable("Geraete");
-                });
-
-            modelBuilder.Entity("FES.AusleihSystem.ViewModels.ReservierungViewModel", b =>
-                {
-                    b.Property<int>("ReservierungsNummer")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<DateTime>("ReservierungsBeginn");
-
-                    b.Property<TimeSpan>("ReservierungsDauer");
-
-                    b.Property<DateTime>("ReservierungsEnde");
-
-                    b.Property<DateTime>("ReservierungsZeitpunkt");
-
-                    b.HasKey("ReservierungsNummer");
-
-                    b.ToTable("Reservierungen");
-                });
-
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole", b =>
-                {
-                    b.Property<string>("Id");
-
-                    b.Property<string>("ConcurrencyStamp")
-                        .IsConcurrencyToken();
-
-                    b.Property<string>("Name")
-                        .HasMaxLength(256);
-
-                    b.Property<string>("NormalizedName")
-                        .HasMaxLength(256);
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("NormalizedName")
-                        .HasName("RoleNameIndex");
-
-                    b.ToTable("AspNetRoles");
-                });
-
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("ClaimType");
-
-                    b.Property<string>("ClaimValue");
-
-                    b.Property<string>("RoleId")
-                        .IsRequired();
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("RoleId");
-
-                    b.ToTable("AspNetRoleClaims");
-                });
-
-            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUser", b =>
+            modelBuilder.Entity("FES.AusleihSystem.Models.ApplicationUser", b =>
                 {
                     b.Property<string>("Id");
 
@@ -142,6 +64,88 @@ namespace FES.AusleihSystem.Migrations
                         .HasName("UserNameIndex");
 
                     b.ToTable("AspNetUsers");
+                });
+
+            modelBuilder.Entity("FES.AusleihSystem.ViewModels.GeraetViewModel", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("EAN");
+
+                    b.Property<int>("GeraeteStatus");
+
+                    b.Property<string>("Name");
+
+                    b.Property<int?>("ReservierungsNummer");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("ReservierungsNummer");
+
+                    b.ToTable("Geraete");
+                });
+
+            modelBuilder.Entity("FES.AusleihSystem.ViewModels.ReservierungViewModel", b =>
+                {
+                    b.Property<int>("ReservierungsNummer")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("NutzerId");
+
+                    b.Property<DateTime>("ReservierungsBeginn");
+
+                    b.Property<TimeSpan>("ReservierungsDauer");
+
+                    b.Property<DateTime>("ReservierungsEnde");
+
+                    b.Property<DateTime>("ReservierungsZeitpunkt");
+
+                    b.HasKey("ReservierungsNummer");
+
+                    b.HasIndex("NutzerId");
+
+                    b.ToTable("Reservierungen");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole", b =>
+                {
+                    b.Property<string>("Id");
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .IsConcurrencyToken();
+
+                    b.Property<string>("Name")
+                        .HasMaxLength(256);
+
+                    b.Property<string>("NormalizedName")
+                        .HasMaxLength(256);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("NormalizedName")
+                        .HasName("RoleNameIndex");
+
+                    b.ToTable("AspNetRoles");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ClaimType");
+
+                    b.Property<string>("ClaimValue");
+
+                    b.Property<string>("RoleId")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("AspNetRoleClaims");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserClaim<string>", b =>
@@ -218,6 +222,13 @@ namespace FES.AusleihSystem.Migrations
                         .HasForeignKey("ReservierungsNummer");
                 });
 
+            modelBuilder.Entity("FES.AusleihSystem.ViewModels.ReservierungViewModel", b =>
+                {
+                    b.HasOne("FES.AusleihSystem.Models.ApplicationUser", "Nutzer")
+                        .WithMany()
+                        .HasForeignKey("NutzerId");
+                });
+
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
                 {
                     b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole")
@@ -228,7 +239,7 @@ namespace FES.AusleihSystem.Migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserClaim<string>", b =>
                 {
-                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUser")
+                    b.HasOne("FES.AusleihSystem.Models.ApplicationUser")
                         .WithMany("Claims")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
@@ -236,7 +247,7 @@ namespace FES.AusleihSystem.Migrations
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserLogin<string>", b =>
                 {
-                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUser")
+                    b.HasOne("FES.AusleihSystem.Models.ApplicationUser")
                         .WithMany("Logins")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
@@ -249,7 +260,7 @@ namespace FES.AusleihSystem.Migrations
                         .HasForeignKey("RoleId")
                         .OnDelete(DeleteBehavior.Cascade);
 
-                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUser")
+                    b.HasOne("FES.AusleihSystem.Models.ApplicationUser")
                         .WithMany("Roles")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade);
